d8800bca5b72d8a44120137c55440b8ea04eb275
atawfique12 <abirtawfique12@outlook.com>
2020-01-08 17:34:02 -0500

    Added typing effect for intro section

--- portfolio/src/Components/Body.js
+++ portfolio/src/Components/Body.js
@@ -15,15 +15,92 @@ class Body extends Component {
           url: "./",
           languages: ["HTML", "CSS", "JavaScript", "Words API"]
         }
-      ]
+      ],
+      typed: false,
+      count: 0
     }
+  }
+
+  typeWriter = () => {
+    let i = 0;
+    let txt = ['an Avid Software Engineer.', "I'm business-minded.", 'a learner.', 'an Entrepreneur.', "I'm detail-oriented.", "I'm creative."];
+    let count = this.state.count;
+    let speed = 100;
+    let currentcomponent = this;
+
+    if (this.state.typed === false) {
+      let typing = setInterval(function () {
+        if (i < txt[count].length) {
+          document.getElementById("input").innerHTML += txt[count].charAt(i);
+          i++;
+        }
+        if (i >= txt[count].length) {
+          count++
+          if (count >= txt.length) {
+            currentcomponent.setState({
+              typed: true,
+              count: 0
+            })
+          } else {
+            currentcomponent.setState({
+              typed: true,
+              count
+            })
+          }
+          clearInterval(typing)
+        }
+      }, speed);
+    }
+  }
+
+
+
+  blinking = () => {
+    setInterval(function () { document.getElementById("cursor").classList.toggle("zeroop"); }, 500)
+  }
+
+  removing = () => {
+    let currentcomponent = this;
+    let typeWriter = this.typeWriter
+
+    if (this.state.typed === true) {
+      setTimeout(() => {
+        let txt = document.getElementById("input").innerHTML
+        let i = txt.length;
+        let speed = 30;
+        function deleter() {
+          if (i >= 0) {
+            txt = txt.slice(0, txt.length - 1)
+            document.getElementById("input").innerHTML = txt
+            i--;
+            setTimeout(deleter, speed);
+          }
+          if (i === 0) {
+            currentcomponent.setState({
+              typed: false
+            })
+            typeWriter()
+          }
+        }
+        deleter()
+      }, 1500)
+    }
+  }
+
+  componentDidMount = () => {
+    this.typeWriter()
+    this.blinking()
+  }
+
+  componentDidUpdate = () => {
+    this.removing()
   }
 
   render() {
     return (
       <div className="content-wrapper" >
         <div className="intro">
-          <h3><span role="img" alt="waving emoji">👋🏼</span> I'm Abir: Lead of Operations at ExigoSource & an <span id="se">Avid Software Engineer</span></h3>
+          <h3><span role="img" alt="waving emoji">👋🏼</span> I'm Abir: Lead of Operations at ExigoSource & <span id="typewriter"><span id="input"></span><span id="cursor">|</span></span></h3>
         </div>
         <div className="projects-wrapper">
           {this.state.projects.map((project, key) =>
--- portfolio/src/Components/Project.js
+++ portfolio/src/Components/Project.js
@@ -4,7 +4,7 @@ const Project = (props) => {
   return (
     <div className="project" key={props.key}>
       <h1>{props.name}</h1>
-      <h4>{props.img}</h4>
+      <img src={props.url}></img>
       <a href={props.url}>See {props.name} Live!</a>
       <div className="language-wrapper">
         <h3>CREATED WITH</h3>
